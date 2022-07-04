511eb0e739920e3974007d44e566c75f3f41e178
wanderer <1379605319@qq.com>
2022-07-04 23:39:15 +0800

    2022/7/4 23:35

---
+++ java/Test.java
@@ -0,0 +1,15 @@
+class City{
+    public String name;
+    public double latitude;
+    public double longitude;
+}
+public class Test{
+    public static void main(String[] args) {
+        City bj = new City();
+        bj.name = "Beijing";
+        bj.latitude = 39.903;
+        bj.longitude = 116.401;
+        System.out.println(bj.name);
+        System.out.println("location: " + bj.latitude + ", " + bj.longitude);
+    }
+}
